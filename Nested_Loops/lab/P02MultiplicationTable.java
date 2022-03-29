e128125497a17cfe2da2858e74303067c962a280
Daniel <daniel.mihovv@gmail.com>
2022-03-29 21:53:11 +0300

    MultiplicationTable problem commit

---
+++ Nested_Loops/lab/P02MultiplicationTable.java
@@ -0,0 +1,14 @@
+package Nested_Loops.lab;
+
+import java.util.Scanner;
+
+public class P02MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 1; i < 11; i++) {
+            for (int j = 1; j < 11; j++) {
+                System.out.printf("%d * %d = %d\n", i, j, i * j);
+            }
+        }
+    }
+}
